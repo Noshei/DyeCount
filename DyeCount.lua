95950c45dee09a69b3a0da1c6c57bd7ab77e8d74
Noshei <8866000+Noshei@users.noreply.github.com>
2026-02-09 23:25:09 -0700

    1.0.0
    Initial release

---
+++ DyeCount.lua
@@ -0,0 +1,49 @@
+---@class DyeCount
+local _, DC = ...
+
+DyeCount = DC
+
+function DC:TRADE_SKILL_SHOW()
+    -- Check to make sure we have the dye crafting window open and recipe list available
+    if not ProfessionsFrame then
+        ChatFrame1:AddMessage("No ProfessionsFrame")
+        return
+    end
+    if not ProfessionsFrame:IsShown() then
+        ChatFrame1:AddMessage("ProfessionsFrame Not Shown")
+        return
+    end
+
+    -- Thanks Plusmouse and Slackluster for this callback / method get data into the recipe list
+    if not DC.TradeskillHook then
+        ProfessionsFrame.CraftingPage.RecipeList.ScrollBox:RegisterCallback("OnAcquiredFrame", function(_, v, data)
+            local recipeInfo = data.data.recipeInfo
+            if recipeInfo then
+                if data.parent.data.categoryInfo and data.parent.data.categoryInfo.skillLineID == 2984 then
+                    local recipeID = recipeInfo.recipeID
+                    if recipeID then
+                        table.insert(DyeCount.test, v)
+                        table.insert(DyeCount.test2, data)
+                        if not v.availableCount then
+                            v.availableCount = v:CreateFontString(nil, "OVERLAY")
+                            v.availableCount:SetFontObject("GameFontHighlight_NoShadow")
+                            v.availableCount:SetHeight(12)
+                            v.availableCount:SetPoint("RIGHT", v, "RIGHT", -5, 0)
+                            v.availableCount:SetText(123456789)
+                        end
+                        local dyeData = C_TooltipInfo.GetRecipeResultItem(recipeInfo.recipeID)
+                        local totalDye = C_Item.GetItemCount(dyeData.id, true, false, true, true)
+                        v.availableCount:SetText(totalDye)
+                    end
+                else
+                    if v.availableCount then
+                        v.availableCount:SetText()
+                    end
+                end
+            end
+        end)
+        DC.TradeskillHook = true
+    end
+end
+
+EventRegistry:RegisterFrameEventAndCallback("TRADE_SKILL_SHOW", DC.TRADE_SKILL_SHOW)
